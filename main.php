8e84220d939f7403783663c2e93681db8076896d
miszczuk-cell <miszczuk.ivan@gmail.com>
2019-10-20 17:09:28 +0200

    Test

---
+++ main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Menu</title>
+  </head>
+  <body>
+    <h1>oghfkoih</h1>
+    <?php include 'include/menu.html'?>
+    <script src="include/jquery.js"></script>
+    <script src="include/script.js"></script>
+  </body>
+</html>
