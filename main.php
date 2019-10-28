20da1c445b855da85d1e21bc47a565b3593f50b0
Jailbait88 <jmasse3576088@gmail.com>
2019-10-28 17:10:50 +0100

    ok

--- main.php
+++ main.php
@@ -3,18 +3,10 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
-    <title>Menu test</title>
+    <title>Menu</title>
   </head>
   <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <h1>Julien ça marche bien</h1>
-=======
-    <h1>Test de GitHub pour voir si ça marche</h1>
->>>>>>> 479ed8707b7ea8db3ae91c13869b4730b1640cd5
-=======
-    <h1>Oui ça marche tkt !</h1>
->>>>>>> c1532ba7a74aaa528dc3e864122fe9ac25432f78
+    <h1>Titre</h1>
     <?php include 'include/menu.html'?>
     <script src="include/jquery.js"></script>
     <script src="include/script.js"></script>
