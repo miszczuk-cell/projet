9dfb855024f4277f2e4b2b2f58bfae824844684a
miszczuk-cell <miszczuk.ivan@gmail.com>
2019-10-28 17:04:16 +0100

    Le commit pour julien

--- main.php
+++ main.php
@@ -3,14 +3,10 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
-<<<<<<< HEAD
-    <title>Bienvenue</title>
-=======
     <title>Menu test</title>
->>>>>>> 1bf852d6de4c32955863502e25e2635679b1a108
   </head>
   <body>
-    <h1>Le commit</h1>
+    <h1>Julien ça marche bien</h1>
     <?php include 'include/menu.html'?>
     <script src="include/jquery.js"></script>
     <script src="include/script.js"></script>
