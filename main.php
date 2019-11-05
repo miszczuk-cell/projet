478e6ee7ef3fe87cec00aea70acbc01fb80465a0
Guillaume <delasalle.sio.onfray.g@gmail.com>
2019-11-05 11:09:22 +0100

    Inversion des menus

--- main.php
+++ main.php
@@ -6,10 +6,10 @@
     <title>Menu</title>
   </head>
   <body>
+    <?php include 'include/menu.html'?>
     <h1>Titre</h1>
     <h1>Oui ça marche tkt !</h1>
     <h1> J'ai vu ça oui c'est nice </h1>
-    <?php include 'include/menu.html'?>
     <script src="include/jquery.js"></script>
     <script src="include/script.js"></script>
   </body>
