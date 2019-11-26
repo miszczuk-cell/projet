67269e4fd84ade8813b835e3835b0c6e61c3320c
Jailbait88 <jmasse3576088@gmail.com>
2019-11-26 13:03:55 +0100

    Suppression Jquery

--- main.php
+++ main.php
@@ -8,8 +8,6 @@
   <body>
     <?php include 'include/menu.html'?>
     <h1>Titre</h1>
-    <h1>Oui ça marche tkt !</h1>
-    <h1> J'ai vu ça oui c'est nice </h1>
     <script src="include/jquery.js"></script>
     <script src="include/script.js"></script>
   </body>
