0d791ce69fec6ebfbf748f18d41ba96c36584e06
Jailbait88 <jmasse3576088@gmail.com>
2019-11-26 19:36:32 +0100

    rrty

---
+++ include/script.js
@@ -0,0 +1,11 @@
+//function disparaitre(var element)
+//{
+//  element.style.display = "none";
+//}
+var i = 0;
+var nbDiv = document.querySelectorAll("li").length;
+while(i<nbDiv)
+{
+  alert(nbDiv[i]);
+  i = i+1;
+}
