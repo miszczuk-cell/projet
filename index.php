1a58f4ab979076e9536e998c44271d5ed7496aff
Jailbait88 <jmasse3576088@gmail.com>
2019-12-19 17:21:54 +0100

    fdsfd

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	</head>
 
 	<body></br></br></br>
+		<div class="textePrésentation">
 		<h1>Présentation</h1>
 			<p>
 				Les frères de la Communion Fraternelle de Jésus Serviteur sont présents au service des plus
@@ -57,6 +58,57 @@
 				<p>
 					Ils vivent du travail de leurs mains et cherchent à manifester leur amour du prochain par le service désintéressé, l’hospitalité, l’accueil et l’aumône.
 				</p>
+			</div>
+			<h1>Présentation</h1>
+				<p>
+					Les frères de la Communion Fraternelle de Jésus Serviteur sont présents au service des plus
+					pauvres et de l’Église, ils veulent demeurer attentifs aux besoins des personnes handicapées
+					et des communautés religieuses qu’ils accompagnent.
+				 </p>
+				 <p>
+					 En suivant le Christ Serviteur, Frère Christian, Frère Marc et Frère Emmanuel désirent
+					 s’inspirer dans leur vie quotidienne d’un passage de l’Évangile de Saint-Jean :</br>
+					 « A ceci, tous vous reconnaitront pour mes disciples : à l’amour que vous aurez les uns pour les autres. »
+				 </p>
+				<h1>Histoire</h1>
+					<p>
+							L’histoire des frères de la Communion Fraternelle commence aux JMJ de Paris en 1997 quand
+							frère Christian, qui est alors responsable de l’accueil international des personnes handicapées,
+							appelle ceux qui deviendront frère Marc et frère Emmanuel à porter avec lui cette
+							responsabilité. 10 000 jeunes sourds, aveugles, handicapés moteurs et mentaux ou
+							gravement malades vivront pleinement ces JMJ. Cette participation massive est une première
+							dans l’histoire des rassemblements catholiques et sera saluée par le Pape Jean-Paul II. Ce
+							fut une expérience fondatrice qui enracina les frères dans leur désir de rester unis pour le
+							service de l’Église.
+					</p>
+					<p>
+							Encouragés par Monseigneur Albert-Marie DE MONLÉON, Monseigneur Gérard DAUCOURT et Monseigneur Renato BOCCARDO
+							à maintenir l’esprit de charité et de communion fraternelle qui les animait, ils commencèrent une vie commune le 8 décembre 1997.</br>
+							Monseigneur DE MONLÉON, alors évêque de Pamiers, leur remit l’habit blanc et l’anneau de consécration le 15 août 1998/
+					</p>
+					<p>
+							Saint Jean-Paul II a reçu les frères, le 26 janvier 2004, et a accordé sa bénédiction à la Communauté.
+					</p>
+					<p>
+							Le 4 décembre 2011, au cours de l’eucharistie, Monseigneur Albert-Marie DE MONLÉON, évêque de Meaux, a approuvé les Constitutions (leur Règle)
+							et a remis aux frères de Jésus-Serviteur le décret de reconnaissance canonique de la communauté. De nombreux fidèles sont venus des divers pôles
+							du diocèse de Meaux pour les accompagner en ce jour d’action de grâce.
+					</p>
+					<p>
+						Les frères de Jésus-Serviteur veulent se mettre à la suite de Jésus Serviteur lavant les pieds de ses disciples « en ayant toujours un amour de prédilection
+						pour les personnes handicapées, âgées ou blessées par la vie, et ils cherchent à les soutenir et à combattre toute forme de pauvreté
+						matérielle et spirituelle. » (extrait des Constitutions)
+					</p>
+					<p>
+						Marqués par leur vocation apostolique de type canonial, les frères mènent une vie commune de caractère familial, toute ordonnées à l’exigence de la charité fraternelle.
+					</p>
+					<p>
+						Leur vie est structurée par le célibat consacré, l’obéissance et la simplicité de vie. Elle est rythmée par les offices à l’oratoire, l’eucharistie et la méditation de la Parole de Dieu.
+					</p>
+					<p>
+						Ils vivent du travail de leurs mains et cherchent à manifester leur amour du prochain par le service désintéressé, l’hospitalité, l’accueil et l’aumône.
+					</p>
+				</div>
 	<?php include ("include/menu.html"); ?>
 	<script src="include/script.js"></script>
 </body>
